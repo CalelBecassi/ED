c17ae3f0c7a3bc7f397ae3691bc79ebf72a3523a
Calel Becassi <calelbecassi@hotmail.com>
2020-04-21 17:39:51 -0400

    FINALIZADO

---
+++ Trabalho1/indice-hash.1.sol
@@ -0,0 +1,48 @@
+0:
+1: olla(1) facom(1,3,5) decode(5)
+2:
+3: funcoes(5)
+4:
+5: trabalhar(4)
+6: dados(3,6)
+7:
+8:
+9: esse(1,2)
+10:
+11: metodos(4) encode(5)
+12:
+13:
+14:
+15:
+16:
+17:
+18:
+19:
+20:
+21:
+22: disciplina(2)
+23: arquivo(1)
+24: utilizamos(5)
+25:
+26:
+27:
+28:
+29:
+30:
+31: urls(5)
+32: teste(1)
+33:
+34:
+35:
+36:
+37: com(4)
+38:
+39:
+40:
+41: estruturas(3,6)
+42:
+43:
+44: para(4) codificacao(4)
+45:
+46: trabalho(2,6)
+Fator de carga: 0.404255
--- Trabalho1/tabDispersão.cpp
+++ Trabalho1/tabDispersão.cpp
@@ -83,7 +83,7 @@ public:
   ~TabelaDispersao(); // Destrutor padrão
   void escreve();     // Escreve a tabela (incluindo fator de carga)
   void novaOcorrencia(char p[], int linha); // Registra uma nova ocorrência de uma palavra
-  void separaPalavras(char linhaTXT[], int contadorLinhas); // Recebe a linha e devolve a palavra em objeto
+  void separaPalavras(char linhaTXT[], int contadorLinhas); //Separa a string linhaTXT em palavras
   float fatorDeCarga;
 
 private:
@@ -98,49 +98,64 @@ int main(int argc, char *argv[]){
   int contadorLinhas = 1;
   char linhaTXT[999999];
 
-  TabelaDispersao *tab = new TabelaDispersao();
+  TabelaDispersao tab;
 
-  FILE *texto = fopen(argv[1], "r");
+  FILE *texto = fopen(argv[1], "r"); //Abrindo arquivo de texto de entrada
   if(texto == NULL)
     printf("***Erro ao abrir o arquivo.txt***\n");
 
-  while(fgets(linhaTXT, sizeof(linhaTXT), texto) != NULL){ //Passando linha por linha do texto.
-    tab->separaPalavras(linhaTXT, contadorLinhas);
+  while(fgets(linhaTXT, sizeof(linhaTXT), texto) != NULL){ //Salvando na string linhaTXT cada linha do texto
+    tab.separaPalavras(linhaTXT, contadorLinhas); //Chamando função pra separar a linha em palavras
     contadorLinhas++;
   }
 
-  tab->escreve();
-
+  tab.escreve();
+  
   fclose(texto);
   return 0;
 }
 
-Ocorrencia::Ocorrencia(int linha){
-  this->linha = linha;
+// ********** MÉTODOS DA CLASSE OCORRENCIA **********
+Ocorrencia::Ocorrencia(int linha){ //Construtor padrão
+  this->linha = linha; //Iniciando o atributo linha da classe
 }
 
-ListaO::ListaO(){
-  this->inicio = NULL;
-  this->fim = NULL;
+Ocorrencia::~Ocorrencia(){} //Destrutor padrão
+
+// ********** MÉTODOS DA CLASSE LISTAO **********
+ListaO::ListaO(){ //Construtor padrão
+  this->inicio = NULL; //Iniciando fazendo com que o ponteiro inicio aponte pra nulo
+  this->fim = NULL; //Iniciando fazendo com que o ponteiro fim aponte pra nulo
 }
 
-void ListaO::insere(int linha){
+ListaO::~ListaO(){ //Destrutor padrão
+  Ocorrencia *olhando;
+
+  olhando = this->inicio;
+  while(olhando != NULL){ //Passando de ocorrencia em ocorrencia na lista desalocando espaço utilizado na memória
+    olhando = olhando->prox;
+    delete(this->inicio);
+    this->inicio = olhando;  
+  }
+}
+
+void ListaO::insere(int linha){ //Insere a ocorrência na lista de ocorrências
   Ocorrencia *olhando;
   
   olhando = this->inicio;
-  while((olhando != NULL) && (olhando->linha != linha)){
+  while((olhando != NULL) && (olhando->linha != linha)){ //Laço pra descobrir se já existe uma ocorrência da palavra na mesma linha
     olhando = olhando->prox;
   }
 
-  if(olhando == NULL){
-    Ocorrencia *novaO = new Ocorrencia(linha);
-    if(this->inicio == NULL){
+  if(olhando == NULL){ //Se olhando == nulo quer dizer que não foi encontrado uma ocorrência da palavra na mesma linha
+    Ocorrencia *novaO = new Ocorrencia(linha); //Instanciando um novo objeto da classe ocorrência
+    if(this->inicio == NULL){ //Caso a lista esteja vazia ainda
       this->inicio = novaO;
       this->fim = novaO;
       novaO->prox = NULL;
     }
   
-    else{
+    else{ //Caso a lista não esteja vazia
       this->fim->prox = novaO;
       this->fim = novaO;
       novaO->prox = NULL;
@@ -153,81 +168,98 @@ void ListaO::escreve(){
 
   olhando = this->inicio;
   printf("(");
-  while(olhando != this->fim){
+  while(olhando != this->fim){ //Laço pra printar ocorrência por ocorrência
     printf("%d,", olhando->linha);
     olhando = olhando->prox;
   }
   printf("%d)", this->fim->linha);
 }
 
-Palavra::Palavra(char p[]){
-  strcpy(this->p, p);
+// ********** MÉTODOS DA CLASSE PALAVRA **********
+Palavra::Palavra(char p[]){ //Construtor padrão
+  strcpy(this->p, p); //Iniciando o atributo p da classe
   this->prox = NULL;
 }
 
-void Palavra::insere(int linha){
-  ocorrencias.insere(linha);
+Palavra::~Palavra(){} //Destrutor padrão
+
+void Palavra::insere(int linha){ //Insere a ocorrência na lista de ocorrências da palavra
+  ocorrencias.insere(linha); 
 }
 
-void Palavra::escreve(){
+void Palavra::escreve(){ //Printa a palavra e suas ocorrências
   printf("%s", this->p);
   ocorrencias.escreve();
 }
 
-ListaP::ListaP(){
-  this->inicio = NULL;
-  this->fim = NULL;
+// ********** MÉTODOS DA CLASSE LISTAP **********
+ListaP::ListaP(){ //Construtor padrão
+  this->inicio = NULL; //Iniciando fazendo com que o ponteiro inicio aponte pra nulo
+  this->fim = NULL; //Iniciando fazendo com que o ponteiro fim aponte pra nulo
 }
-ListaP::~ListaP(){}
 
-Palavra *ListaP::insere(char p[]){
+ListaP::~ListaP(){ //Destrutor padrão
+  Palavra *olhando;
+
+  olhando = this->inicio;
+  while(olhando != NULL){ //Passando de palavra em palavra na lista desalocando espaço utilizado na memória
+    olhando = olhando->prox;
+    delete(this->inicio);
+    this->inicio = olhando;  
+  }
+}
+
+Palavra *ListaP::insere(char p[]){ //Insere a palavra na lista de palavras
   Palavra *novaP = new Palavra(p);
 
-  if(this->inicio == NULL){
+  if(this->inicio == NULL){ //Caso a lista esteja vazia
     this->inicio = novaP;
     this->fim = novaP;
     novaP->prox = NULL;
   }
 
-  else{
+  else{ //Caso a lista não esteja vazia
     this->fim->prox = novaP;
     this->fim = novaP;
     novaP->prox = NULL;
   }
+
   return novaP;
 }
 
-Palavra *ListaP::busca(char p[]){
+Palavra *ListaP::busca(char p[]){ //Busca a palavra na lista de palavras, devolve um ponteiro pro objeto caso encontre e devolve nulo caso não encontre
   Palavra *olhando;
 
   olhando = this->inicio;
-  while((olhando != NULL) && (strcmp(p, olhando->p) != 0)){
+  while((olhando != NULL) && (strcmp(p, olhando->p) != 0)){ //Verificando palavra por palavra se é a palavra que está buscando
     olhando = olhando->prox;
   }
 
-  if(olhando != NULL)
+  if(olhando != NULL) //Se olhando != nulo quer dizer que a palavra foi encontrada na lista
     return olhando;
   else
     return NULL;
 }
 
-void ListaP::escreve(){
+void ListaP::escreve(){ //Escreve a lista de palavras com suas respectivas ocorrências
   Palavra *olhando;
 
   olhando = this->inicio;
-  while(olhando != NULL){
+  while(olhando != NULL){ //Verificando se a lista não está vazia
     printf(" ");
     olhando->escreve();
     olhando = olhando->prox;
   }
 }
 
-TabelaDispersao::TabelaDispersao(){
-  fatorDeCarga = 0;
+// ********** MÉTODOS DA CLASSE TABELADISPERSAO **********
+TabelaDispersao::TabelaDispersao(){ //Construtor padrão
+  fatorDeCarga = 0; //Iniciando o atributo fatorDeCarga da classe
 }
-TabelaDispersao::~TabelaDispersao(){}
 
-void TabelaDispersao::escreve(){
+TabelaDispersao::~TabelaDispersao(){} //Destrutor padrão
+
+void TabelaDispersao::escreve(){ //Escreve a tabela com cada lista de palavras e suas ocorrências em cada linha de seu hash 
   for(int i = 0; i < TAMANHO; i++){
     printf("%d:", i);
     tabela[i].escreve();
@@ -238,43 +270,43 @@ void TabelaDispersao::escreve(){
 }
 
 void TabelaDispersao::novaOcorrencia(char p[], int linha){
-  if((tabela[h(p)].busca(p) == NULL) && (strlen(p) >= MIN)){
-    tabela[h(p)].insere(p)->insere(linha);
+  if((tabela[h(p)].busca(p) == NULL) && (strlen(p) >= MIN && strlen(p) <= MAX)){ //Caso a palavra ainda não tenha sido inserida, e ela tenha mais que 3 letras e menos que 20 letras
+    tabela[h(p)].insere(p)->insere(linha); //Insere a palavra na sua respectiva lista de palavras e sua linha na qual foi encontrada
     fatorDeCarga++;
   }
 
-  else if(strlen(p) >= MIN){
-    tabela[h(p)].busca(p)->insere(linha);
+  else if(strlen(p) >= MIN && strlen(p) <= MAX){ //Caso a palavra ja tenha sido inserida e garantindo que ela tenha mais que 3 letras e menos que 20 letras
+    tabela[h(p)].busca(p)->insere(linha); //Insere mais uma ocorrência na lista de ocorrências daquela palavra
   }
 }
 
-void TabelaDispersao::separaPalavras(char linhaTXT[], int contadorLinhas){
+void TabelaDispersao::separaPalavras(char linhaTXT[], int contadorLinhas){ //Separa a string linhaTXT em palavras
   char *p;
   int tamanhoP;
 
-  p = strtok (linhaTXT," ,.-");
+  p = strtok (linhaTXT," ,.-!\n"); //Aponta do inicio da linha até o primeiro espaço, vírgula, ponto, hífen ou ponto de exclamação
   while (p != NULL)
   {
     tamanhoP = strlen(p);
 
-    for(int i = 0; i < tamanhoP; i++) //Laço para converter letras maiúsculas para letras minúsculas
+    for(int i = 0; i < tamanhoP; i++) //Laço para converter letras maiúsculas em letras minúsculas
       p[i] = tolower(p[i]); 
 
-    novaOcorrencia(p, contadorLinhas);
-    p = strtok (NULL, " ,.-\n");
+    novaOcorrencia(p, contadorLinhas); //Passa a palavra encontrada pra função novaOcorrencia e qual linha ela está no texto
+    p = strtok (NULL, " ,.-!\n"); //Aponta do próximo caracter depois de onde parou que não seja espaço, vírgula, ponto, hífen ou ponto de exclamação até o próximo espaço, vírgula, ponto, hífen ou ponto de exclamação 
   }
 }
 
-int TabelaDispersao::valor(char p[]){
+int TabelaDispersao::valor(char p[]){ //Função para calcular o valor da palavra p
   int valor = 0;
 
-  for(int i = 0; p[i] != '\n' && p[i] != '\0'; i++){
+  for(int i = 0; p[i] != '\n' && p[i] != '\0'; i++){ //Somando valor de cada caractere da palavra
     valor = valor + p[i];
   }
 
   return valor;
 }
 
-int TabelaDispersao::h(char p[]){
+int TabelaDispersao::h(char p[]){ //Função para calcular o hash da palavra p
   return valor(p) % 47;
 }
